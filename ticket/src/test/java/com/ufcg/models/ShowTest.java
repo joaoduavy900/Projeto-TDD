0b49f3e516f517985ec31c6dbf9ec1277421c407
davihsg <daviandlion@gmail.com>
2024-07-31 09:57:25 -0300

    feat(show): add test skeletons

---
+++ ticket/src/test/java/com/ufcg/models/ShowTest.java
@@ -0,0 +1,80 @@
+package com.ufcg.models;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ShowTest extends TestCase {
+  public ShowTest(String testName) {
+    super(testName);
+  }
+
+  public static Test suite() {
+    return new TestSuite(ShowTest.class);
+  }
+
+  /** Test constructor */
+  public void testConstructor() {
+    fail();
+  }
+
+  /** Test constructor null date */
+  public void testConstructorNullDate() {
+    fail();
+  }
+
+  /** Test constructor empty artist */
+  public void testConstructorEmptyArtist() {
+    fail();
+  }
+
+  /** Test constructor null artist */
+  public void testConstructorNullArtist() {
+    fail();
+  }
+
+  /** Test constructor negative fee */
+  public void testConstructorNegativeFee() {
+    fail();
+  }
+
+  /** Test constructor negative cost */
+  public void testConstructorNegativeCost() {
+    fail();
+  }
+
+  /** Test constructor empty ticket batches */
+  public void testConstructorEmptyTicketBatches() {
+    fail();
+  }
+
+  /** Test constructor duplicated tickets */
+  public void testConstructorDuplicatedTickets() {
+    fail();
+  }
+
+  /** Test get report */
+  public void testGetReport() {
+    fail();
+  }
+
+  /** Test get report special date */
+  public void testGetReportSpecialDate() {
+    fail();
+  }
+
+  /** Test get status loss */
+  public void testGetStatusLoss() {
+    fail();
+  }
+
+  /** Test status stable */
+  public void testGetStatusStable() {
+    fail();
+  }
+
+  /** Test status profit */
+  public void testGetStatusProfit() {
+    fail();
+  }
+}
